b8ba1853101f17104b51c669be81c6f3ac33338e
waltralex63 <waltralex63@gmail.com>
2018-08-10 11:49:02 -0600

    Primera fase

---
+++ app/Post.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+
+    protected $fillable=[
+        'user_id','category_id','name','slug','excerpt','body','status','file'
+    ];
+    public function user()
+    {
+        return $this->belongsTo(User::Tag);
+    }
+    public function category()
+    {
+        return $this->belongsTo(Category::Tag);
+    }
+
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
+}
